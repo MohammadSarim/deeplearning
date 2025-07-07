683774be66e633fa322cdb0c8bffce3bc702b2e5
Mohmmad Sarim <mohdsarim8@gmail.com>
2025-07-07 07:46:33 +0530

    hidden layers MLP

--- MLP_MNIST.py
+++ MLP_MNIST.py
@@ -1,25 +1,29 @@
 import time
-from torchvision import datasets, transforms
+import numpy as np
+from torchvision import datasets
+from torchvision import transforms
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch
 import matplotlib.pyplot as plt
-import matplotlib
+import pandas as pd
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+##########################
+### SETTINGS
+##########################
 
-# ------------------------ Hyperparameters ------------------------
-random_seed = 123
-learning_rate = 0.1
-num_epochs = 25
-batch_size = 256
+RANDOM_SEED = 1
+BATCH_SIZE = 100
+NUM_EPOCHS = 100
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-num_features = 784  # 28x28 flattened image
-num_classes = 10
 
-torch.manual_seed(random_seed)
+##########################
+### MNIST DATASET
+##########################
 
-# ------------------------ Dataset ------------------------
+# Note transforms.ToTensor() scales input images
+# to 0-1 range
 train_dataset = datasets.MNIST(root='data', 
                                train=True, 
                                transform=transforms.ToTensor(),
@@ -29,61 +33,133 @@ test_dataset = datasets.MNIST(root='data',
                               train=False, 
                               transform=transforms.ToTensor())
 
+
 train_loader = DataLoader(dataset=train_dataset, 
-                          batch_size=batch_size, 
+                          batch_size=BATCH_SIZE, 
                           shuffle=True)
 
 test_loader = DataLoader(dataset=test_dataset, 
-                         batch_size=batch_size, 
+                         batch_size=BATCH_SIZE, 
                          shuffle=False)
 
-# ------------------------ Checking Batch Dimensions ------------------------
+# Checking the dataset
 for images, labels in train_loader:  
-    print('Image batch dimensions:', images.shape)  # NCHW format
-    print('Label batch dimensions:', labels.shape)
+    print('Image batch dimensions:', images.shape)
+    print('Image label dimensions:', labels.shape)
     break
 
-# ------------------------ Model Definition ------------------------
-
 class MLP(torch.nn.Module):
+
     def __init__(self, num_features, num_hidden, num_classes):
-        super(MLP, self).__init__()
+        super().__init__()
         
-        ### First Hidden Layer
         self.num_classes = num_classes
-        self.linear1 = torch.nn.Linear(num_features, num_hidden)
-        self.linear1.weight.data.detach.normal(0, 0.1)
-        self.linear1.bias.data.zero_()
-
-        ### Output Layer
-        self.linear_out = torch.nn.Linear(num_hidden, num_classes)
-        self.linear_out.weight.data.detach.normal(0, 0.1)
-        self.linear1.bias.data.zero_()
-
-    def forward(self, X):
-        out = self.linear1(X)
-        out = torch.sigmoid(out)
-
-        logits = self.linear_out(out)
-        probas = torch.softmax(logits, dim = 1)
-        return logits, probas
-    
-model = MLP(num_features, num_classes).to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-# ------------------------ Accuracy Function ------------------------
-def compute_accuracy(model, data_loader):
-    correct_pred, num_examples = 0, 0
-    
-    for features, targets in data_loader:
-        features = features.view(-1, 28*28).to(device)
-        targets = targets.to(device)
-        logits, probas = model(features)
-        _, predicted_labels = torch.max(probas, 1)
-        num_examples += targets.size(0)
-        correct_pred += (predicted_labels == targets).sum()
         
-    return correct_pred.float() / num_examples * 100
+        ### 1st hidden layer
+        self.linear_1 = torch.nn.Linear(num_features, num_hidden)
+        self.linear_1.weight.detach().normal_(0.0, 0.1)
+        self.linear_1.bias.detach().zero_()
 
-# ------------------------ Training ------------------------
+        ### Output layer
+        self.linear_out = torch.nn.Linear(num_hidden, num_classes)
+        self.linear_out.weight.detach().normal_(0.0, 0.1)
+        self.linear_out.bias.detach().zero_()
+        
+    def forward(self, x):
+        out = self.linear_1(x)
+        out = torch.sigmoid(out)
+        logits = self.linear_out(out)
+        #probas = torch.softmax(logits, dim=1)
+        return logits#, probas
 
+    
+#################################
+### Model Initialization
+#################################
+    
+torch.manual_seed(RANDOM_SEED)
+model = MLP(num_features=28*28,
+            num_hidden=100,
+            num_classes=10)
+
+model = model.to(DEVICE)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+#################################
+### Training
+#################################
+
+def compute_loss(net, data_loader):
+    curr_loss = 0.
+    with torch.no_grad():
+        for cnt, (features, targets) in enumerate(data_loader):
+            features = features.view(-1, 28*28).to(DEVICE)
+            targets = targets.to(DEVICE)
+            logits = net(features)
+            loss = F.cross_entropy(logits, targets)
+            curr_loss += loss
+        return float(curr_loss)/cnt
+
+
+
+start_time = time.time()
+minibatch_cost = []
+epoch_cost = []
+for epoch in range(NUM_EPOCHS):
+    model.train()
+    for batch_idx, (features, targets) in enumerate(train_loader):
+        
+        features = features.view(-1, 28*28).to(DEVICE)
+        targets = targets.to(DEVICE)
+            
+        ### FORWARD AND BACK PROP
+        logits = model(features)
+        
+        cost = F.cross_entropy(logits, targets)
+        optimizer.zero_grad()
+        
+        cost.backward()
+       
+        ### UPDATE MODEL PARAMETERS
+        optimizer.step()
+        
+        ### LOGGING
+        minibatch_cost.append(cost.item())
+        if not batch_idx % 50:
+            print ('Epoch: %03d/%03d | Batch %03d/%03d | Cost: %.4f' 
+                   %(epoch+1, NUM_EPOCHS, batch_idx, 
+                     len(train_loader), cost.item()))
+        
+    cost = compute_loss(model, train_loader)
+    epoch_cost.append(cost)
+    print('Epoch: %03d/%03d Train Cost: %.4f' % (
+            epoch+1, NUM_EPOCHS, cost))
+    print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+    
+print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+
+plt.plot(range(len(minibatch_cost)), minibatch_cost)
+plt.ylabel('Cross Entropy')
+plt.xlabel('Minibatch')
+plt.show()
+
+plt.plot(range(len(epoch_cost)), epoch_cost)
+plt.ylabel('Cross Entropy')
+plt.xlabel('Epoch')
+plt.show()
+
+def compute_accuracy(net, data_loader):
+    correct_pred, num_examples = 0, 0
+    with torch.no_grad():
+        for features, targets in data_loader:
+            features = features.view(-1, 28*28).to(DEVICE)
+            targets = targets.to(DEVICE)
+            logits = net.forward(features)
+            predicted_labels = torch.argmax(logits, 1)
+            num_examples += targets.size(0)
+            correct_pred += (predicted_labels == targets).sum()
+        return correct_pred.float()/num_examples * 100
+    
+print('Training Accuracy: %.2f' % compute_accuracy(model, train_loader))
+print('Test Accuracy: %.2f' % compute_accuracy(model, test_loader))
